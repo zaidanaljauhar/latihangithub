d32351902cd7eb76e48526911b02e87ea15cad7d
zaidanaljauhar <zaidan_23520002@stimata.ac.id>
2023-11-16 21:40:11 +0700

    tugas8.php terbaru baris 7-8

--- tugas_index.php/index.php
+++ tugas_index.php/index.php
@@ -1,19 +1,4 @@
 <?php
 
-var_dump(1 < 5) ;
-    echo "</br>"; //output baris ke-1
-
-    var_dump(1 > 5) ;
-    echo "</br>"; //output baris ke-2
-
-    var_dump(1 <= 5) ;
-    echo "</br>"; //output baris ke-3
-
-    var_dump(1 >= 5) ;
-    echo "</br>"; //output baris ke-4
-
-    var_dump(1 == 5) ;
-    echo "</br>"; //output baris ke-5
-
-    var_dump(1 != 5) ;
-    echo "</br>"; //output baris ke-6
+var_dump (1 === "1"); //output baris ke-7
+var_dump (1 !== "1"); //output baris ke-8
