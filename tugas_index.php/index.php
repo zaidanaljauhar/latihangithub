0a5af7e6f17208ae2ec5765f0f8314b4051cca9e
zaidanaljauhar <zaidan_23520002@stimata.ac.id>
2023-11-16 21:21:57 +0700

    tugas7.php terbaru semuanya muncul

--- tugas_index.php/index.php
+++ tugas_index.php/index.php
@@ -3,25 +3,21 @@
 $x = 10;
 $x += 2; 
 
-echo $x "</br>";
-
 $y = 10;
 $y -= 2;
-
-echo $y "</br>";
 
 $z = 10;
 $z /= 2;
 
-echo $z "</br>"
-
 $a = 10;
 $a %= 2;
-
-echo $a "</br>"
 
 $b = 10;
 $b .= 2;
 
-echo $b "</br>"
 
+echo $x . "</br>";
+echo $y . "</br>";
+echo $z . "</br>";
+echo $a . "</br>";
+echo $b . "</br>";
