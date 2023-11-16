d0e034c2db97d10e660999c2f828376590b97f10
zaidanaljauhar <zaidan_23520002@stimata.ac.id>
2023-11-16 22:01:26 +0700

    tugas8.php terbaru semua baris muncul

--- tugas_index.php/index.php
+++ tugas_index.php/index.php
@@ -1,7 +1,29 @@
 <?php
+//operator perbandingan
+var_dump (1 < 5);
+echo "</br>";       #output baris ke-1
+var_dump (1 > 5);
+echo "</br>";       #output baris ke-2
+var_dump (1 <= 5);
+echo "</br>";       #output baris ke-3
+var_dump (1 >= 5);
+echo "</br>";       #output baris ke-4
+var_dump (1 == 5);
+echo "</br>";       #output baris ke-5
+var_dump (1 != 5);
+echo "</br>";       #output baris ke-6
 
+//operator identitas
+var_dump (1 === 5); #output baris ke-7
+echo "</br>"; 
+var_dump (1 !== 5); #output baris ke-8
+
+//operator logika
+echo "</br>"; 
 $x = 10;
-var_dump ($x < 20 && $x % 2 == 0); //output baris ke-9
-var_dump ($x < 20 II $x % 2 == 0); //output baris ke-10
+echo var_dump ($x < 20 && $x % 2 == 0) . "</br>";   #output baris ke-9
+$x = 10;
+var_dump ($x < 20 || $x % 2 == 0);  #output baris ke-10
+echo var_dump ($x < 20 || $x % 2 == 0) . "</br>";   #output baris ke-10
 
 ?>
