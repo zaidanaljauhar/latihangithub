a5d0aae125063487a253a437c927c77a9c561b5e
zaidanaljauhar <zaidan_23520002@stimata.ac.id>
2023-11-16 19:19:38 +0700

    tugas6.php

--- tugas_index.php/index.php
+++ tugas_index.php/index.php
@@ -1,11 +1,8 @@
 <?php
-//Operator Aritmatika
-$x = 10;
-$y = 20;
-    echo $x + $y ;  #operator penjumlahan
-    echo $x - $y ;  #operator pengurangan
-    echo $x * $y ;  #operator perkalian
-    echo $x / $y ;  #operator pembagian
-    echo $x % $y ;  #operator modulus
+
+$nama_depan = "Mohammad";
+$nama_belakang = "Zaidan";
+
+echo $nama_depan . "" . $nama_belakang;
 
 ?>
