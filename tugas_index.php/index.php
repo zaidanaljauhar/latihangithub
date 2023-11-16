7f61aecbe3b28109175a9dcebce5286ba713e078
zaidanaljauhar <zaidan_23520002@stimata.ac.id>
2023-11-16 18:32:53 +0700

    tugas1.php

---
+++ tugas_index.php/index.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>
+            Belajar PHP
+        </title>
+    </head>
+    <body>
+        <h1>
+            Selamat Datang <?php echo "masukkan nama anda";?>
+        </h1>
+    </body>
+</html>
