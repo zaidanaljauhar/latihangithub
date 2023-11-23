51cc7a78339e1b2e864a25bc7894e56a8998bec3
zaidanaljauhar <zaidan_23520002@stimata.ac.id>
2023-11-23 21:56:30 +0700

    task5.php

--- latihan1.php/latihan1.php
+++ latihan1.php/latihan1.php
@@ -1,6 +1,8 @@
 <?php
-$i = 1;
-while ($i <= 5) {
-    echo"Hai, ini baris ke- <br>";
-    $i++;
-}
+echo "<h2>Menggunakan perulangan Do While</h2>";
+$i = 10;
+do {
+    echo "Hello world <br>";
+$i++;
+    } while ($i < 5);
+//muncul satu kali karena i memiliki nilai lebih besar dari 5
