5c0bf30d92641c07f42509731f3450cdc7d8df6d
zaidanaljauhar <zaidan_23520002@stimata.ac.id>
2023-11-24 09:52:31 +0700

    task10.php

--- tugas_perulangan/latihan3.php
+++ tugas_perulangan/latihan3.php
@@ -1,9 +1,15 @@
 <?php
-$x = 20;
-if ($x < 10) {
-    echo "benar";
-} else if ($x==20) {
-    echo "binggo!";
-} else {
-    echo "salah";
+$x = 60;
+if ($x < 50) {
+    echo "Nilai Kamu Adalah E";
+} else if ($x < 60) {
+    echo "Nilai Kamu Adalah D";
+} else if ($x < 70) {
+    echo "Nilai Kamu Adalah C";
+} else if ($x < 80) {
+    echo "Nilai Kamu Adalah B";
+} else if ($x < 90) {
+    echo "Nilai Kamu Adalah A";
+}else {
+    echo "Nilai Kamu Adalah A+";
 }
