13853bd1fef598e8547afc751f204ac8993a785a
zaidanaljauhar <zaidan_23520002@stimata.ac.id>
2023-11-24 09:05:07 +0700

    task6.php

---
+++ tugas_perulangan/latihan2.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan Looping</title>
+</head>
+<body>
+    <h1>Membuat perulangan tabel</h1>
+    <table border="1" cellpadding="10" cellspacing="0">
+        <?php
+        for ($b = 1; $b <= 5; $b++) { #b untuk baris
+            echo "<tr>";
+            for ($k = 1; $k <= 5; $k++) { #k untuk kolom
+                echo "<td> $b,$k </td>";
+            }
+            echo "</tr>";
+        }
+        ?>
+    </table>
+</body>
+</html>
