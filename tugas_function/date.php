ef53e37cd209444e3ecb258ff40d1d216765105b
zaidanaljauhar <zaidan_23520002@stimata.ac.id>
2023-12-07 14:16:19 +0700

    task_3

--- tugas_function/date.php
+++ tugas_function/date.php
@@ -1,20 +1,57 @@
 <?php
-$todayDate = "07 des 2024";
-$returnDate = date('d-M-Y', strtotime('+100 day',strtotime($todayDate)));
-    ?>
+
+// Deklarasi variabel
+$tanggalSekarang = new DateTime();
+$tanggalMasaDepan;
+
+// Menampilkan tanggal hari ini
+echo "Tanggal hari ini: " . $tanggalSekarang->format("d-m-Y") . PHP_EOL;
+
+// Perulangan untuk 1000 hari yang akan datang
+for ($i = 0; $i < 1000; $i++) {
+    // Menambahkan 1 hari ke tanggal sekarang
+    $tanggalMasaDepan = $tanggalSekarang->add(new DateInterval("P1D"));
+
+    // Menampilkan tanggal di masa depan
+    $hari = $tanggalMasaDepan->format("l");
+    $tanggal = $tanggalMasaDepan->format("d");
+    $bulan = $tanggalMasaDepan->format("m");
+    $tahun = $tanggalMasaDepan->format("Y");
+
+    // Menampilkan output HTML
+    echo "<tr>";
+    echo "<td>" . $i + 1 . "</td>";
+    echo "<td>" . $hari . "</td>";
+    echo "<td>" . $tanggal . "</td>";
+    echo "<td>" . $bulan . "</td>";
+    echo "<td>" . $tahun . "</td>";
+    echo "</tr>";
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>task 2</title>
+    <title>Output Hari, Tanggal, Bulan, dan Tahun</title>
 </head>
-
 <body>
-    <h1>
-    tanggal hari ini = <?= $todayDate; ?>
-    <br>
-    tanggal +100 hari ini sekarang = <?= $returnDate; ?>
-    </h1>
+    <table border="1">
+        <thead>
+            <tr>
+                <th>No</th>
+                <th>Hari</th>
+                <th>Tanggal</th>
+                <th>Bulan</th>
+                <th>Tahun</th>
+            </tr>
+        </thead>
+        <tbody>
+        <?php
+        // Menampilkan output PHP
+        ?>
+        </tbody>
+    </table>
 </body>
 </html>
