77e787476717f7d8ce814fe163cb05a681be3db3
zaidanaljauhar <zaidan_23520002@stimata.ac.id>
2023-12-07 14:06:27 +0700

    task_2

--- tugas_function/date.php
+++ tugas_function/date.php
@@ -1,6 +1,20 @@
 <?php
-//data untuk menampilkan tanggal dengan format tertentu
+$todayDate = "07 des 2024";
+$returnDate = date('d-M-Y', strtotime('+100 day',strtotime($todayDate)));
+    ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>task 2</title>
+</head>
 
-echo date("1");
-
-?>
+<body>
+    <h1>
+    tanggal hari ini = <?= $todayDate; ?>
+    <br>
+    tanggal +100 hari ini sekarang = <?= $returnDate; ?>
+    </h1>
+</body>
+</html>
