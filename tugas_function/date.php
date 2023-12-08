aec95baaeae39206e8c9a8cb9bb608e919f8c2e1
zaidanaljauhar <zaidan_23520002@stimata.ac.id>
2023-12-08 10:12:03 +0700

    task_6

--- tugas_function/date.php
+++ tugas_function/date.php
@@ -1,26 +1,32 @@
 <?php
 
-//deklarasi variabel 
-$harilahir = mktime(0,0,0,4,28,2002);
+function 
+hari_ulang_tahun($tahun,
+$bulan, $tanggal) {
+    //membuat timestamp dari tanggal ulang tahun menggunakan mktime
+$timestamp_ulang_tahun = mktime(0,0,0, $bulan, $tanggal, $tahun);
 
-//menampilkan tanggal lahir 
-$hari = date ("1",$minggu);
-$tanggal = date ("d",$28);
-$bulan = date ("m",$04);
-$tahun = date ("y",$2002);
+    //mengambil nama hari dari timestamp
+$hari_ulang_tahun = date("1", $timestamp_ulang_tahun);  
 
-echo "Tanggal lahir":"$minggu","28","04","2002"
+    return 
+$hari_ulang_tahun;
+}
+
+//menentukan tanggal ulang tahun
+$tanggal_ulang_tahun = 28;
+$bulan_ulang_tahun = 4;
+$tahun_ulang_tahun = 2002;
+
+//menentukan tahun untuk mencari hari ulang tahun 
+$tahun_target = 2030;
+
+//memanggil fungsi untuk menemukan hari ulang tahun 
+$hasil_hari_ulang_tahun = 
+hari_ulang_tahun($tahun_target, $bulan_ulang_tahun, $tanggal_ulang_tahun);
+
+echo "Hari ulang tahun pada tanggal 
+$tanggal_ulang_tahun-$bulan_ulang_tahun $tahun_target adalah 
+$hasil_hari_ulang_tahun.";
 
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>output hari lahir</title>
-</head>
-<body>
-    <h2>output hari lahir</h2>
-    <p>tanggal lahir: <?php echo $hari "minggu", $tanggal "28", $bulan "04", $tahun "2002"; ?></p>
-</body>
-</html>
