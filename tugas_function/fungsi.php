302e8afb7f2076c6c6b13989e01c8d9d35149885
zaidanaljauhar <zaidan_23520002@stimata.ac.id>
2023-12-08 10:38:00 +0700

    task_8

---
+++ tugas_function/fungsi.php
@@ -0,0 +1,12 @@
+<?php
+//pembuatan fungsi
+function perkalian($angka1,$angka2)
+{
+    return "perkalian $waktu, $nama!";
+}
+
+//pemanggilan fungsi
+$hasil = perkalian(20,3);
+echo "Perkalian 20 x 3 adalah $hasil"; 
+echo "<br>";
+echo "Perkalian 9 x 2 adalah " . perkalian(9,2);
