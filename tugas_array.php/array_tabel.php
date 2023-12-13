b379b806be24f280fd30216d38b36cb359ce1336
zaidanaljauhar <zaidan_23520002@stimata.ac.id>
2023-12-13 11:36:27 +0700

    arraytabel

---
+++ tugas_array.php/array_tabel.php
@@ -0,0 +1,42 @@
+<?php
+$angka = [3,2,5,15,20,77,89];
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>array 2</title>
+    <style>
+        .kotak{
+            width: 50px;
+            height: 50px;
+            background-color: salmon;
+            text-align: center;
+            line-height: 50px;
+            margin: 3px;
+            float: left;
+        }
+        .clear{
+            clear: both;
+        }
+    </style>
+</head>
+<body>
+     <!--<?php for($i=o; $i < 7;$i++){ ?>
+      <div class="kontak">
+        <?php $angka[$i]; ?>
+      </div>
+    <?php }; ?> -->
+        <div class="clear"></div>
+        <?php foreach ($angka as $a) : ?>
+            <div class="kontak"><?= $a; ?></div>
+            <?php endforeach ?>
+            <div class="clear"></div>
+            <?php foreach ($angka as $a) : ?>
+            <div class="kotak"><?= $a; ?></div>
+            <?php endforeach ?>
+            <div class="clear"></div>
+</body>
+</html>
