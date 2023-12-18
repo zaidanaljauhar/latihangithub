ca54564563b9c9283002dcf075d91c635a1344b9
zaidanaljauhar <zaidan_23520002@stimata.ac.id>
2023-12-18 10:23:29 +0700

    latihan1array.php

---
+++ tugas_minggu8.php/latihan1.php
@@ -0,0 +1,11 @@
+<?php
+$hari = array("rabu", "kamis", "jumat");
+$bulan = ["januari", "februari", "maret"];
+$gabungan = [100, "teks", true];
+
+var_dump($hari);
+echo "<br>";
+print_r($bulan);
+echo "<br>";
+
+echo $gabungan[0];
