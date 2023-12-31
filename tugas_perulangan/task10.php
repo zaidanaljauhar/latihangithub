67792c09e5829679eb5052dbbafbf0a400240ca0
zaidanaljauhar <zaidan_23520002@stimata.ac.id>
2023-12-31 13:28:19 +0700

    task10

---
+++ tugas_perulangan/task10.php
@@ -0,0 +1,15 @@
+<?php
+$x = 100;
+if ($x < 50) {
+    echo "Nilai Kamu Adalah E";
+} else if ($x < 60) {
+    echo "Nilai Kamu Adalah D";
+} else if ($x < 70) {
+    echo "Nilai Kamu Adalah C";
+} else if ($x < 80) {
+    echo "Nilai Kamu Adalah B";
+} else if ($x < 90) {
+    echo "Nilai Kamu Adalah A";
+}else {
+    echo "Nilai Kamu Adalah A+";
+}
